b2b015017cd7db5ca32214a1acdd11511a2fb58b
saveroo <saveroo27@gmail.com>
2020-07-25 07:49:18 +0700

    feat(utils/db): fauna wrapper

---
+++ utils/db.ts
@@ -0,0 +1,72 @@
+﻿import faunadb, { Client, ClientConfig, query } from 'faunadb';
+import { SRFApis } from '../types/SRFApis';
+
+const q = query;
+
+// const {decrypted} = require("../../util/crypt")
+// const fs = require("fs");
+// const _ = require("lodash");
+// const json = require("../../util/utils");
+
+// const secret: string = "fnADxVYOkQACEngPSzJ3jq0d7VJ2btleWzRFPs6H";
+const secret = process.env.FAUNADB_SECRET || process.env.FAUNADB_SERVER_KEY;
+
+const opts: ClientConfig = {
+  secret,
+  domain: 'db.fauna.com',
+  scheme: 'https',
+};
+
+class dbClass {
+  private client: faunadb.Client;
+  private q: any;
+  private readonly collection: string;
+
+  constructor(client: Client, q: any) {
+    this.client = client;
+    this.q = q;
+    this.collection = 'sr_users';
+  }
+
+  async create(dt: SRFApis.IFromFauna) {
+    const query = await this.client.query(
+      this.q.Create(this.q.Collection(this.collection), { data: dt })
+    );
+    return query;
+  }
+
+  async createWithId(id: string, dt: any) {
+    return await this.client.query(
+      this.q.Create(this.q.Ref(this.q.Collection(this.collection), id), {
+        data: dt,
+      })
+    );
+  }
+
+  async updateByRef(refId: number, dt: any) {
+    return await this.client.query(
+      this.q.Update(this.q.Ref(q.Collection(this.collection), refId), {
+        data: dt,
+      })
+    );
+  }
+
+  async getIndexByTerms(indexName: string, terms: string) {
+    return await this.client.query(
+      this.q.Map(
+        this.q.Paginate(this.q.Match(this.q.Index(indexName), terms)),
+        this.q.Lambda('deviceId', this.q.Get(this.q.Var('deviceId')))
+      )
+    );
+  }
+
+  async get(id: string) {
+    await this.client.query(
+      this.q.Get(this.q.Ref(this.q.Collection(this.collection), id))
+    );
+  }
+}
+
+const client = new faunadb.Client(opts);
+
+export default new dbClass(client, q);
