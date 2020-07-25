524a9d5509a0b60c1299fcf2538ce87bbf1eaa0f
saveroo <saveroo27@gmail.com>
2020-07-25 07:51:13 +0700

    feat(API): opiniated data feeder

---
+++ api/data/index.ts
@@ -0,0 +1,23 @@
+﻿﻿import {NowRequest, NowResponse} from '@vercel/node/dist';
+import crypt from '../../utils/crypt';
+import json from '../../utils/loaders';
+import guard from '../../utils/guard';
+
+// TODO: Check Version properly
+// /check/version
+module.exports = async (request: NowRequest, response: NowResponse) => {
+  guard(request, response).then(async (condition) => {
+    const data = await json;
+    if(condition){
+      const stringify = JSON.stringify(data)
+      const encryptedBody = crypt.encrypted('SRFramework', stringify);
+      return response.status(200).json({
+        status: response.statusCode,
+        author: crypt.salt(),
+        body: encryptedBody,
+      });
+    }
+  }).catch((err) => {
+    return response.status(400).send(err);
+  })
+}
