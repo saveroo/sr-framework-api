bd4ad321d19da6bca01b4dd83cc9f0fef03d8f0a
saveroo <saveroo27@gmail.com>
2020-07-25 08:12:43 +0700

    refactor(util/crypter)!: tidy & put to .env instead

--- utils/crypt.ts
+++ utils/crypt.ts
@@ -1,21 +1,20 @@
 ﻿﻿import CryptoJS, {WordArray} from 'crypto-js';
-import jsonfile from 'jsonfile';
 import { createHmac } from 'crypto';
 
-// const GenerateRandomSalt = () => {
-//   return crypto.randomBytes(32);
-// }
+const {CRYPT_SALT: _salt} = process.env;
 
-const testData = '/yzj08pCqhxV/7qjjPcs+XLHg+lwh5YMbHXHYCp4DIxZeE2sGtkCVuQcIwB0vKQs5924VA+eImNHMf/Yt+p9C6qXvRlYHqVMpwfZNsANrpaXF+ASjT7XEOkpyIJAGwYhfxJpQVrtCx6WbVtDZhhvmaD5GQm796Jf216TXYoFKHveb+VzHGtbyvIqhutvyWgGj3tgZEi/qDfoikl4/uewV9dc4vdc68ijN89sB6kN/L0yjARZd/PAS+sDJ17/GH5gefpZRLzD4KNVxB0+pnj70To33VreqaPsUrQmXtc8TD9kfiVSYT94Tg/4/qdUITQXXsZrtWrEASpQUTNoWUH14fz/8XFHuiL/f/hvjs/7+sXxhIHzRLutaP3D/2aQFlv7sgffdavMeY+U5spvbx7pUhbRirk5zx2QBidiNx/X59UnuyfWvGGDzG53X3JxKhxS5uv6fdISvH+ujgsiuaKrWyYwQVfMsWnjCYh4Oh2GP/vyePfTEzb1Bh4mcJ2KiPzVfZozt22X1k0mOM20vwoHgRHxwm05icaTd4pmG2/FGpUh0xLcOjTW21op43sqFaJJfMJRg7APVAcP60TPf9+Kg6HGj9PvI1BuS3IwXzRtGr3WbD6x3J5tDoY0Nd8eIFU7vL2P2X7yo1l8ybbpWsLd9VeXFCoHLNLFo/11xVPBywYa9hnevIctM9YL73Dn8VIPOApWTfmodZXe+n7qIMRSKsxGxbPyY+2oiZ1ex1oRb6ziMVOrvvvQgBHQ87fBuEhmHTX3PW4vnE0W30xu1G41mgN9oXDUve4BMBzl98ZFggEeB1HOrzlPFVCp0uSeIi7IvIFFAITN/0RbVum6t2MWCyX6mwLFDyBireD6KStp+ojGdSY9yZwm2rUjuCiedAt4Ufa46ckl6Jb1LLSt04JxBLFo4c4bIJ7+tqYumgHEXXsUu6t7y7jkdGEnp3Jyf1eg';
-const _salt = 'MuhammadSurgaSavero';
-
+/**
+ * @param {string} secretKey (key to lock)
+ * @param {string} data (to be encrypted)
+ * @returns {CryptoJS.WordArray}
+ */
 function EncryptText(secretKey: string, data: string): CryptoJS.WordArray {
   const keySize = 256;
   const blockSize = 128;
 
   const pass: string = CryptoJS.enc.Utf8.parse(secretKey);
   // const salt: string = CryptoJS.lib.WordArray.random(8);
-  const salt2: string = CryptoJS.enc.Utf8.parse(_salt);
+  const salt2: string = CryptoJS.enc.Utf8.parse(_salt as string);
 
   const text = CryptoJS.enc.Utf8.parse(data);
   const KeyBytes =  CryptoJS.PBKDF2(pass, salt2, {
@@ -45,8 +44,12 @@ function EncryptText(secretKey: string, data: string): CryptoJS.WordArray {
 
   return encryptedText;
 }
-// let tt = jsonfile.readFileSync(`${__dirname}/../data/SRFeature.json`);
 
+/**
+ * @param {string} secretKey (a key to unlock)
+ * @param {*} data to be decrypted (in base 64)
+ * @returns {CryptoJS.WordArray} return [xx, xx]
+ */
 function DecryptText(secretKey: string, data: any): CryptoJS.WordArray {
   const keySize = 256;
   const blockSize = 128;
@@ -55,7 +58,7 @@ function DecryptText(secretKey: string, data: any): CryptoJS.WordArray {
   const pass: any = CryptoJS.enc.Utf8.parse(secretKey);
   // const b64 = Buffer.from(data, 'base64');
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const salt2: any = CryptoJS.enc.Utf8.parse(_salt);
+  const salt2: any = CryptoJS.enc.Utf8.parse(_salt as string);
   // const text = CryptoJS.enc.Utf8.parse(b64.toString());
   const KeyBytes =  CryptoJS.PBKDF2(pass, salt2, {
     keySize: 48,
@@ -82,16 +85,11 @@ function DecryptText(secretKey: string, data: any): CryptoJS.WordArray {
   });
 }
 
-// console.log(DecryptText("SRFramework", testData).toString(CryptoJS.enc.Utf8));
-
 export default {
   salt: () => {
     return _salt;
   },
   encrypted: (secret: string, dt: any) => {
-
-    // const json: any = jsonfile.readFileSync(`${__dirname}/../data/SRFeature.json`);
-    // const str: string = JSON.stringify(json);
     const result = EncryptText(secret, dt);
     return result.toString();
   },
