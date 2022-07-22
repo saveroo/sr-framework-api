18b7b8ac0387183285e35b08da5a92c4f424ebf8
Muhammad Surga Savero <saveroo27@gmail.com>
2022-07-22 19:31:21 +0700

    chore(deps/config): added @cheerio deps

--- quasar.conf.js
+++ quasar.conf.js
@@ -54,7 +54,8 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
-      distDir: ctx.mode.spa ? 'public/dist' : null,
+      // distDir: ctx.mode.spa ? 'public/dist' : null,
+      distDir: ctx.mode.spa ? 'public' : null, // VERCEL
       // transpile: false,
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
