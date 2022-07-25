5be3e18b5c05a3114985c2fef0659e8b0fe43941
Muhammad Surga Savero <saveroo27@gmail.com>
2022-07-25 09:13:25 +0700

    ci(vercel):  fix deployments path

--- quasar.config.js
+++ quasar.config.js
@@ -66,7 +66,7 @@ module.exports = configure(function (ctx) {
         browser: [ 'es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1' ],
         node: 'node16'
       },
-      vueRouterMode: 'history', // available values: 'hash', 'history'
+      vueRouterMode: 'hash', // available values: 'hash', 'history'
       // distDir: ctx.mode.spa ? 'public/dist' : null,
       distDir: ctx.mode.spa ? 'public' : null, // VERCEL
       // transpile: false,
